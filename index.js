03ab7a4286cd5dcac4b7e4845002354f4da50d46
kael <yorts52@qq.com>
2015-10-24 21:23:14 +0800

    update

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var server = require('http');
 var unzip = require('unzip');
 var Stream = require('stream');
 var config = require('./config');
+var package = require('./package');
 var formidable = require('formidable');
 var dateFormat = require('dateformat');
 var querystring = require('querystring');
@@ -13,7 +14,7 @@ var querystring = require('querystring');
 var program = require('commander');
 
 program
-    .version('0.1.5')
+    .version(package.version)
     .usage('[options] <value ...>')
     .option('-o, --output [./maps]', 'unzip output folder')
     .option('-p, --port <80>', 'server listen port', parseInt)
@@ -33,6 +34,10 @@ server.createServer(function(request, response) {
 
     console.log(dateFormat(new Date(), "yyyy-mm-dd hh:MM:ss"), host, pathname, param.src);
 
+    var pathname_match = function(path) {
+        return new RegExp('^' + path + '(\\.map)?$', 'i').test(pathname);
+    };
+
     var response_end = function(data, status_code) {
         response.writeHead(status_code || (data === '404' ? 404 : 200), {
             'Content-Type': 'text/plain'
@@ -41,45 +46,41 @@ server.createServer(function(request, response) {
     };
 
     // upload
-    if (pathname === '/upload') {
+    if (pathname_match('/upload') && request.method.toLowerCase() == 'post') {
         var form = new formidable.IncomingForm();
         form.encoding = 'binary';
-        form.maxFieldsSize = 1024 * 1024 * 1024; // 1024MB
+        form.maxFieldsSize = 1024 * 1024 * 1024; // 1024 MB
         form.parse(request, function(err, fields) {
-            if (err) return response_end('error', 403);
+            if (err) return response_end('parse error', 403);
 
             if (fields && fields.zip) {
-
                 var data = new Buffer(fields.zip.length);
                 data.write(fields.zip, 0, 'binary');
-
                 var zip_data = new Stream.PassThrough();
                 zip_data.end(new Buffer(data));
                 zip_data.pipe(unzip.Extract({
                     path: output
                 }));
-
-                return response_end('success');
+                return response_end('data success');
+            } else {
+                return response_end('data error');
             }
         });
     } else {
         var real_path = '';
-        if (pathname === '/load' && param.src) {
+        if (pathname_match('/load') && param.src) {
             real_path = output + param.src.replace(/^(https?:)?(\/\/)?/i, '/');
         } else {
             real_path = output + host.replace(/^(map\.)?/i, '/') + pathname;
         }
+        
         console.info('Real path:', real_path);
+
         fs.exists(real_path, function(exists) {
-            if (!exists) {
-                return response_end('404');
-            }
+            if (!exists) return response_end('404');
+
             fs.readFile(real_path, function(err, file) {
-                if (err) {
-                    return response_end('404');
-                } else {
-                    return response_end(file);
-                }
+                return err ? response_end('404') : response_end(file);
             });
         });
     }
